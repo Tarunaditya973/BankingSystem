8f6e32f51607836060f44f9e4ed0e528a2b632a6
Tarunaditya973 <tarunaditya973@gmail.com>
2024-05-25 23:37:00 +0530

    update routes for accounts

--- routes/accounts.routes.js
+++ routes/accounts.routes.js
@@ -6,11 +6,13 @@ const {
   deposit,
   getBalance,
   withdraw,
+  transfer,
 } = require("../controllers/accounts.controller");
 
 router.post("/createaccount", createAccount);
 router.post("/deposit", deposit);
 router.get("/getbalance", getBalance);
 router.post("/withdraw", withdraw);
+router.post("/transfer", transfer);
 
 module.exports = router;
